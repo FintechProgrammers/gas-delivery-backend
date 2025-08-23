740be143f3dd5f362c8e1d7d2a69825e85a15ac6
DEv Noel <ndamjoh@gmail.com>
2025-08-23 16:34:42 +0100

    user search updated

--- app/Http/Controllers/Admin/UserManagementController.php
+++ app/Http/Controllers/Admin/UserManagementController.php
@@ -30,12 +30,16 @@ class UserManagementController extends Controller
 
         $search = $request->filled('search') ? $request->search : null;
         $status = $request->filled('status')  ? $request->status : null;
-        $accountType = $request->filled('account_type') ? $request->account_type : null;
+        // $accountType = $request->filled('account_type') ? $request->account_type : null;
 
         $query = User::where('is_business', true)->withTrashed();
 
         $query = $query
-            ->when(!empty($search), fn($query) => $query->where('name', 'LIKE', "%{$search}%")->orWhere('email', 'LIKE', "%{$search}%")->orWhere('username', 'LIKE', "%{$search}%"))
+            ->when(
+                !empty($search),
+                fn($query) => $query->where('name', 'LIKE', "%{$search}%")->orWhere('email', 'LIKE', "%{$search}%")
+                // ->orWhere('username', 'LIKE', "%{$search}%")
+            )
             ->when(!empty($status), fn($query) => $query->where('status', $status))
             ->when(!empty($dateFrom) && !empty($dateTo), fn($query) => $query->whereBetween('created_at', [$dateFrom, $dateTo]))
             ->when(!empty($status) && !empty($dateFrom) && !empty($dateTo), fn($query) => $query->where('status', $status)->whereBetween('created_at', [$dateFrom, $dateTo]));
